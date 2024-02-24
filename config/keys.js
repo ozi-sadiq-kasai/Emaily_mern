f1f8f0b746d6504f57df02f70e8b896a87409263
ozi <ozisadiqkasai@gmail.com>
2024-02-24 21:08:37 +0100

    keys cofiguration changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if(process.eventNames.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
  // we are in production - return production keys
  module.exports = require('./prod')
 }else{
